cb86671a6836d0bd5a76706f2c27582d811de187
Marceau RODRIGUES <marceau0707@gmail.com>
2024-05-23 16:30:43 +0200

    Màj Trainees

--- src/Controller/V1/TraineeController.php
+++ src/Controller/V1/TraineeController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller\V1;
 
+use App\Entity\Trainee;
 use App\Repository\TraineeRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/v1/trainees')]
@@ -23,15 +26,27 @@ class TraineeController extends AbstractController
         );
     }
 
-    #[Route('/{id}', name: 'app_trainee_get', methods: ['GET'])]
-    public function getOne(TraineeRepository $traineeRepository, SerializerInterface $serializer, string|int $id): JsonResponse
+    #[Route('/trainee/{id}', name: 'app_trainee_get', methods: ['GET'])]
+    public function getTrainee(TraineeRepository $traineeRepository, SerializerInterface $serializer, int $id): JsonResponse
     {
-        return $this->json(
-            [
-                'success' => true,
-                'data' => json_decode($serializer->serialize($traineeRepository->findOneBy(['id' => $id]), 'json'), true),
-            ],
-            status: 200
-        );
+        $trainee = $traineeRepository->find($id);
+
+        if ($trainee instanceof Trainee) {
+            return $this->json(
+                [
+                    'success' => true,
+                    'data' => json_decode($serializer->serialize($trainee, 'json'), true),
+                ],
+                status: 200
+            );
+        } else {
+            return $this->json(
+                [
+                    'success' => false,
+                    'data' => [],
+                ],
+                status: 403
+            );
+        }
     }
 }
